2446665ba8ab81529f8ebdbf7df4f5f32ab26eb3
Fadel Matar <fadel.matar@gmail.com>
2025-10-04 12:18:33 -0400

    added products photo

--- admin/products/products_list.php
+++ admin/products/products_list.php
@@ -23,7 +23,7 @@ $result = mysqli_query($connect, $query);
 
 <table border="1">
     <tr>
-        
+        <th></th>
         <th>Title</th>
         <th>Description</th>
         <th>Price</th>
@@ -37,12 +37,14 @@ $result = mysqli_query($connect, $query);
     <?php while($record = mysqli_fetch_assoc($result)): ?>
 
         <tr>
+            <td> <?php if($record['product_photo']): ?><img src="<?php echo $record['product_photo']; ?>" width="200"><?php endif; ?> </td>
+
             <td> <?php echo $record['product_title']; ?></td>
             <td> <?php echo $record['product_desc']; ?></td>
             <td> <?php echo $record['product_price']; ?></td>
             <td> <?php echo $record['product_stock']; ?></td>
             <td> <?php echo $record['product_size']; ?></td>
-            <td> No Photo yet</td>
+            <td> <a href="products_photo.php?product_id=<?php echo $record['product_id']; ?>">Photo</a></td>
 
             <td>
                 <a href="products_edit.php?product_id=<?php echo $record['product_id']; ?>">Edit</a>

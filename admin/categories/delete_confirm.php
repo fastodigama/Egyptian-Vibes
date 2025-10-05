3ac3b6f0391378b1213f7ab529c5d36a8743e474
Fadel Matar <fadel.matar@gmail.com>
2025-10-05 14:41:03 -0400

    added category Delete confirm

--- admin/categories/delete_confirm.php
+++ admin/categories/delete_confirm.php
@@ -9,13 +9,13 @@ secure();
 if (isset($_GET['delete'])) {
     $id = intval($_GET['delete']); 
         //Fetch the username
-    $query = "SELECT first, last from users WHERE id = $id LIMIT 1";
+    $query = "SELECT category_name from category WHERE category_id = $id LIMIT 1";
     $result = mysqli_query($connect, $query);
-     $user = mysqli_fetch_assoc($result);
-      $name = $user['first'] . " " . $user['last'];
+     $category = mysqli_fetch_assoc($result);
+      $category_name = $category['category_name'];
 } else {
-    set_message("user not found");
-    header('Location: users_list.php');
+    set_message("category not found");
+    header('Location: category_list.php');
     
     exit;
 }
@@ -29,7 +29,7 @@ if (isset($_GET['delete'])) {
     <title>Confirm Deletion</title>
 </head>
 <body>
-    <h2>Are you sure you want to delete this user <?php echo $name; ?>?</h2>
+    <h2>Are you sure you want to delete this category <?php echo $category_name; ?>?</h2>
 
     <form method="post" action="">
         <input type="hidden" name="id" value="<?php echo $id; ?>">
@@ -42,11 +42,11 @@ if (isset($_GET['delete'])) {
 <?php
 if (isset($_POST['confirm_delete'])) {
     $id = intval($_POST['id']);
-    $query = "DELETE FROM users WHERE id = {$id} LIMIT 1";
+    $query = "DELETE FROM category WHERE category_id = {$id} LIMIT 1";
     mysqli_query($connect, $query);
     // Optional: set_message() if you have a flash message system
-    set_message("User has been deleted");
-    header('Location: users_list.php');
+    set_message("Category has been deleted");
+    header('Location: category_list.php');
     exit;
 }
 ?>

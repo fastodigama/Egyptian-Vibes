c4ffe6f4334f427b19890bcf59a908e02136ad57
Fadel Matar <fadel.matar@gmail.com>
2025-10-05 20:29:05 -0400

    added category update for product_edit

--- admin/products/product_edit.php
+++ admin/products/product_edit.php
@@ -7,15 +7,17 @@ secure();
 
 include('../../includes/header.php');
 
+$id = (int)$_GET['product_id'];
+
 if (isset($_POST['product_title'])) {
     // Sanitize inputs
-    $id     = (int)$_GET['product_id'];
     $title  = mysqli_real_escape_string($connect, $_POST['product_title']);
     $desc   = mysqli_real_escape_string($connect, $_POST['product_desc']);
     $price  = (float)$_POST['product_price'];
     $stock  = (int)$_POST['product_stock'];
     $size   = mysqli_real_escape_string($connect, $_POST['product_size']);
 
+    // Update product
     $query = "UPDATE product SET
                 product_title = '$title',
                 product_desc  = '$desc',
@@ -24,15 +26,24 @@ if (isset($_POST['product_title'])) {
                 product_size  = '$size'
               WHERE product_id = $id
               LIMIT 1";
-
     mysqli_query($connect, $query);
+
+    // Update category links
+    mysqli_query($connect, "DELETE FROM product_category WHERE product_id = $id");
+
+    if (!empty($_POST['category_ids'])) {
+        foreach ($_POST['category_ids'] as $cat_id) {
+            $cat_id = (int)$cat_id;
+            mysqli_query($connect, "INSERT INTO product_category (product_id, category_id) VALUES ($id, $cat_id)");
+        }
+    }
 
     set_message('Product has been updated');
     header('Location: product_list.php');
     die();
 }
-
 ?>
+
 
 <h2>Edit Product</h2>
 
@@ -48,6 +59,8 @@ if (!$record) {
     die();
 }
 ?>
+
+
 
 <form action="" method="POST">
     <div>
@@ -78,6 +91,41 @@ if (!$record) {
         Stock:
         <input type="number" name="product_stock" value="<?php echo (int)$record['product_stock']; ?>">
     </div>
+    <?php
+    // Fetch all categories
+    $category_query = "SELECT * FROM category ORDER BY category_name ASC";
+    $category_result = mysqli_query($connect, $category_query);
+
+    // Fetch linked category IDs
+    $linked_query = "SELECT category_id FROM product_category WHERE product_id = $id";
+    $linked_result = mysqli_query($connect, $linked_query);
+    $linked_ids = [];
+    while ($record = mysqli_fetch_assoc($linked_result)) {
+        $linked_ids[] = $record['category_id'];
+}
+?>
+    <div>
+    <fieldset>
+        <legend>Categories (select one or more)</legend>
+        <?php while ($category = mysqli_fetch_assoc($category_result)): ?>
+            <div>
+                <label>
+                    <?php
+                    $checked = '';
+                    foreach ($linked_ids as $linked_id) {
+                        if ($linked_id == $category['category_id']) {
+                            $checked = 'checked';
+                            break;
+                        }
+                    }
+                    ?>
+                    <input type="checkbox" name="category_ids[]" value="<?php echo $category['category_id']; ?>" <?php echo $checked; ?>>
+                    <?php echo htmlspecialchars($category['category_name']); ?>
+                </label>
+            </div>
+        <?php endwhile; ?>
+    </fieldset>
+</div>
     <input type="submit" value="Update Product">
     <a href="product_list.php"><button type="button">Cancel</button></a>
 </form>

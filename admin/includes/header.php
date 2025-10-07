bc6fd9bf85201d6649bb24835f42c599832c1e61
Fadel Matar <fadel.matar@gmail.com>
2025-10-07 18:04:04 -0400

    modified footer

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -11,7 +11,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
   <!-- <link rel="stylesheet" href="css/style.css"> -->
 </head>
-<body class="bg-light">
+<body class="bg-light d-flex flex-column min-vh-100">
 
   <header class="navbar navbar-expand-lg navbar-dark bg-dark" role="banner">
     <div class="container-fluid">

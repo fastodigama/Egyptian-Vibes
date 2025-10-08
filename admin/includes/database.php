c7f58018bc0c5d714b9c0fb2542b4563b4e0e16e
Fadel Matar <fadel.matar@gmail.com>
2025-10-08 17:09:52 -0400

    modified db connection for railway

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,3 +1,15 @@
 <?php
-$connect = mysqli_connect('localhost', 'root', 'root', 'egyptian_vibes');
+// includes/config.php
+
+$host = getenv("MYSQLHOST") ?: "127.0.0.1";
+$port = getenv("MYSQLPORT") ?: 3306;
+$db   = getenv("MYSQLDATABASE") ?: "egyptian_vibes";
+$user = getenv("MYSQLUSER") ?: "root";
+$pass = getenv("MYSQLPASSWORD") ?: "root";
+
+$connect = new mysqli($host, $user, $pass, $db, $port);
+
+if ($connect->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
 ?>
